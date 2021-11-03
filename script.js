cac40835ca21bcc1b9ffb70d94391f6d782dd015
agmorrow <morrow.austin5@gmail.com>
2021-11-03 01:24:49 -0600

    added pseudocode

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword () {
+  console.log ("Hey you clicked the button!")
 
+// 1. Prompt the user for the password criteria
+//    a. password length 8 < 128
+//    b. Lowercase, uppercase, numbers, special characters
+// 2. Validate the input
+// 3. Generate password based on criteria
+
+
+
+
+// 4 .Display password to the page. 
+
+return "Generated password will go here!" ;
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  
 
   passwordText.value = password;
 
