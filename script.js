82d7597fb799b68014264c332322e0a7306a5039
agmorrow <morrow.austin5@gmail.com>
2021-11-17 21:27:37 -0700

    got the generate button to work

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+let characterLength, lowerCase, upperCase, numeric, specialCharacters
+
 
 function generatePassword() {
-// 1. Prompt the user for the password criteria
-  const characterLength = window.prompt('Please enter the length you wish your password to be');
-//    a. password length 8 < 128
-if (characterLength < 8 || characterLength > 128) {
-  alert('Please enter a password length of at least 8 characters and no more than 128 characters')
+  const passwordType = "";
+  if (lowerCase === true) {
+    passwordType = "abcdefghijklmnopqrstuvwxyv";
+  } else if (upperCase === true) {
+    passwordType = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  //   const retVal = "";
+  //   for (var i = 0; i < length; i++) {
+  //     retVal += generatePassword();
+  //   }
+  //   return retVal;
 }
-//    b. Lowercase, uppercase, numeric, and/or special characters
-  const lowerCase = confirm('Click OK if you would like lowercase characters in your password');
-  const upperCase = confirm('Click OK if you would like uppercase characters in your password');
-  const numeric = confirm('Click OK if you would like numeric characters in your password');
-  const specialCharacters = confirm('Click OK if you would like special characters in your password');
-// 2. Validate the input
-// 3. Generate password based on criteria
-// 4 .Display password to the page. 
-
-  return "Generated password will go here!"
-}
-
-
-
-
 
 // Write password to the #password input
 // Stores the value of generate Password into the Password variable
 function writePassword() {
+  characterLength = window.prompt('Please enter the length you wish your password to be');
+  // if (characterLength < 8 || characterLength > 128) {
+  //   alert('Please enter a password length of at least 8 characters and no more than 128 characters');
+  // } else {
+
+  // }
+  console.log(characterLength);
+  lowerCase = confirm('Click OK if you would like lowercase characters in your password');
+  upperCase = confirm('Click OK if you would like uppercase characters in your password');
+  numeric = confirm('Click OK if you would like numeric characters in your password');
+  specialCharacters = confirm('Click OK if you would like special characters in your password');
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// 1. Prompt the user for the password criteria
+//    a. password length 8 < 128
+//    b. Lowercase, uppercase, numeric, and/or special characters
+// 2. Validate the input
+// 3. Generate password based on criteria
+// 4 .Display password to the page. 
