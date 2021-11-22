fb4e99a3af34b4137ae21461a37bebcc60d8766a
agmorrow <morrow.austin5@gmail.com>
2021-11-21 22:18:03 -0700

    password generator works

--- script.js
+++ script.js
@@ -4,65 +4,53 @@ let characterType = {
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   numeric: "0123456789",
-  specialCharacters: "!@#$%^&*()_+~\\`|}{[]:;?><,./-=",
+  specialCharacters: " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
 };
 
-  
-  
 
 function generatePassword() {
-  var passwordCharSet = "";
-
-  var characterLength = window.prompt("Enter a number from 8 to 128 for password length.");
-
-  var lowerCase = window.confirm("Would you like to use lowercase letters?");
+  // 1. Prompt the user for the password criteria
+  var passwordCriteria = "";
+  //    a. password length between 8 && 128
+  var characterLength = prompt("Enter a number from 8 to 128 for password length.");
+  //    b. Lowercase, uppercase, numeric, and/or special characters
+  var lowerCase = confirm("Click OK if you would you like to use lowercase letters?");
   if (lowerCase) {
-    passwordCharSet += characterType.lowerCase;
+    passwordCriteria += characterType.lowerCase;
   };
 
-  var upperCase = window.confirm("Would you like to use UPPERCASE letters?");
+  var upperCase = confirm("Click OK if you would you like to use UPPERCASE letters?");
   if (upperCase) {
-    passwordCharSet += characterType.upperCase;
+    passwordCriteria += characterType.upperCase;
   };
 
-  var specialCharacters = window.confirm("Would you like to use symbols?");
+  var specialCharacters = confirm("Click OK if you would you like to use special characters?");
   if (specialCharacters) {
-    passwordCharSet += characterType.specialCharacters;
+    passwordCriteria += characterType.specialCharacters;
   };
 
-  var numeric = window.confirm("Would you like to use numbers?");
+  var numeric = confirm("Click OK if you would you like to use numbers?");
   if (numeric) {
-    passwordCharSet += characterType.numeric;
+    passwordCriteria += characterType.numeric;
   };
-  var password = "";
+  // 2. Validate the input
+  // 3. Generate password based on criteria
+  var securePassword = "";
   for (let i = 0; i < characterLength; i++) {
-    password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
+    securePassword += passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)]
   }
-  return password;
+
+  // 4 .Display password to the page. 
+  return securePassword;
 }
-
-console.log(generatePassword());
-
-
-
 
 // Write password to the #password input
 // Stores the value of generate Password into the Password variable
 function writePassword() {
- 
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 };
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-// 1. Prompt the user for the password criteria
-//    a. password length 8 < 128
-//    b. Lowercase, uppercase, numeric, and/or special characters
-// 2. Validate the input
-// 3. Generate password based on criteria
-// 4 .Display password to the page. 
